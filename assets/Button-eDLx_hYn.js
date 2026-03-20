426fc0a6f27b616844e26d6ea800343ed4706c95
rishirajsaini <34911789+rishirajsaini@users.noreply.github.com>
2026-03-20 16:28:23 +0530

    Updates

--- assets/Button-eDLx_hYn.js
+++ assets/Button-eDLx_hYn.js
@@ -1,17 +1,17 @@
-import{j as f}from"./jsx-runtime-BjG_zV1W.js";import{r as v}from"./index-B3TfwC44.js";import{c as y}from"./cx-2dOUpm6k.js";const g=v.forwardRef(function({bsPrefix:e="btn",className:a,unstyled:r,variant:t="primary",size:n,block:i,loading:l,loadingText:o="Loading...",disabled:u,children:s,type:d="button",...m},c){const p=r?a:y(e,`${e}-${t}`,n?`${e}-${n}`:void 0,i?"w-100":void 0,a);return f.jsx("button",{ref:c,type:d,className:p,disabled:u||l,...m,children:l?o:s})});g.__docgenInfo={description:"",methods:[],displayName:"Button",props:{bsPrefix:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'btn'",computed:!1}},className:{required:!1,tsType:{name:"string"},description:""},unstyled:{required:!1,tsType:{name:"boolean"},description:""},variant:{required:!1,tsType:{name:"union",raw:`| 'primary'\r
-| 'secondary'\r
-| 'success'\r
-| 'danger'\r
-| 'warning'\r
-| 'info'\r
-| 'light'\r
-| 'dark'\r
-| 'link'\r
-| 'outline-primary'\r
-| 'outline-secondary'\r
-| 'outline-success'\r
-| 'outline-danger'\r
-| 'outline-warning'\r
-| 'outline-info'\r
-| 'outline-light'\r
+import{j as f}from"./jsx-runtime-BjG_zV1W.js";import{r as v}from"./index-B3TfwC44.js";import{c as y}from"./cx-2dOUpm6k.js";const g=v.forwardRef(function({bsPrefix:e="btn",className:a,unstyled:t,variant:i="primary",size:n,block:r,loading:l,loadingText:o="Loading...",disabled:u,children:s,type:d="button",...m},c){const p=t?a:y(e,`${e}-${i}`,n?`${e}-${n}`:void 0,r?"w-100":void 0,a);return f.jsx("button",{ref:c,type:d,className:p,disabled:u||l,...m,children:l?o:s})});g.__docgenInfo={description:"",methods:[],displayName:"Button",props:{bsPrefix:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'btn'",computed:!1}},className:{required:!1,tsType:{name:"string"},description:""},unstyled:{required:!1,tsType:{name:"boolean"},description:""},variant:{required:!1,tsType:{name:"union",raw:`| 'primary'
+| 'secondary'
+| 'success'
+| 'danger'
+| 'warning'
+| 'info'
+| 'light'
+| 'dark'
+| 'link'
+| 'outline-primary'
+| 'outline-secondary'
+| 'outline-success'
+| 'outline-danger'
+| 'outline-warning'
+| 'outline-info'
+| 'outline-light'
 | 'outline-dark'`,elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'success'"},{name:"literal",value:"'danger'"},{name:"literal",value:"'warning'"},{name:"literal",value:"'info'"},{name:"literal",value:"'light'"},{name:"literal",value:"'dark'"},{name:"literal",value:"'link'"},{name:"literal",value:"'outline-primary'"},{name:"literal",value:"'outline-secondary'"},{name:"literal",value:"'outline-success'"},{name:"literal",value:"'outline-danger'"},{name:"literal",value:"'outline-warning'"},{name:"literal",value:"'outline-info'"},{name:"literal",value:"'outline-light'"},{name:"literal",value:"'outline-dark'"}]},description:"",defaultValue:{value:"'primary'",computed:!1}},size:{required:!1,tsType:{name:"union",raw:"'sm' | 'lg'",elements:[{name:"literal",value:"'sm'"},{name:"literal",value:"'lg'"}]},description:""},block:{required:!1,tsType:{name:"boolean"},description:""},loading:{required:!1,tsType:{name:"boolean"},description:""},loadingText:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:"",defaultValue:{value:"'Loading...'",computed:!1}},type:{defaultValue:{value:"'button'",computed:!1},required:!1}}};export{g as B};
